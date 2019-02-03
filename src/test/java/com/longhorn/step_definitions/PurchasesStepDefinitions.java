22db510a25de08f9de0f79a93aa77133109cbab7
ibrahimsever <isevertx@gmail.com>
2019-02-02 23:19:29 -0600

    Basic inital package and files created, and one scenario added

---
+++ src/test/java/com/longhorn/step_definitions/PurchasesStepDefinitions.java
@@ -0,0 +1,67 @@
+package com.longhorn.step_definitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PurchasesStepDefinitions {
+
+
+    @Given("domain address")
+    public void domain_address() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user go the page")
+    public void user_go_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("home page should be displayed")
+    public void home_page_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("user on the login page")
+    public void user_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user login as a manager")
+    public void user_login_as_a_manager() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user enter valid email")
+    public void user_enter_valid_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user enter valid password")
+    public void user_enter_valid_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user click login button")
+    public void user_click_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("user should see inbox title")
+    public void user_should_see_inbox_title() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+
+
+}
